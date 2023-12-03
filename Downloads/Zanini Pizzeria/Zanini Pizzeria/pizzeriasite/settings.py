509114dbba9f59bcc4e7de9769a557e7048484d5
Luiz Smania <luizsmania1@gmail.com>
2023-12-03 11:24:24 +0000

    Added static root to settings py

--- Downloads/Zanini Pizzeria/Zanini Pizzeria/pizzeriasite/settings.py
+++ Downloads/Zanini Pizzeria/Zanini Pizzeria/pizzeriasite/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'pizzeriasite.urls'
 
-TEMPLATES = [
+EMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [BASE_DIR / "templates"],
@@ -72,7 +72,6 @@ TEMPLATES = [
         },
     },
 ]
-
 WSGI_APPLICATION = 'pizzeriasite.wsgi.application'
 
 
@@ -122,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
